ed8f0033c6fb133fdf0d88f9ed8067edb4f8ffa1
Duvan  Navarro <dnavarro@daabon.com.co>
2022-07-21 16:00:30 -0500

    minimal changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,10 @@
 const board = document.getElementById('board');
 let div = document.createElement('div');
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+for (let i = 0; i < 16; i++) 
+{
+    for (let j = 0; j < 16; j++) 
+    {
         let div = document.createElement('div');
         div.className = `row-${i}`;
         board.appendChild(div);
