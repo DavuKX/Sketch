97c5c760f4037225dc9ad40acdcca864cbe8b4a9
Duvan  Navarro <dnavarro@daabon.com.co>
2022-07-21 16:23:06 -0500

    add grid

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,13 @@
 const board = document.getElementById('board');
 let div = document.createElement('div');
 
-for (let i = 0; i < 16; i++) 
+for (let i = 0; i < 64; i++) 
 {
-    for (let j = 0; j < 16; j++) 
+    for (let j = 0; j < 64; j++) 
     {
         let div = document.createElement('div');
-        div.className = `row-${i}`;
+        div.style.gridRow = i;
+        div.style.gridColumn = j;
         board.appendChild(div);
     }
 }
