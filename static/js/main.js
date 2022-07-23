8f053c0c7e981a5b1ba0ef72a5d2d7355693ec8f
DavuKX <duvandavid2001@gmail.com>
2022-07-23 15:15:40 -0500

    make the javascript

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,44 @@
-const board = document.getElementById('board');
-let div = document.createElement('div');
+const btn         = document.getElementById('reset');
+const grid        = document.getElementById('board');
+const gridValue   = document.getElementById('grid-value');
+const colorPicker = document.getElementById('color-picker');
+const random      = document.getElementById('random');
 
-for (let i = 0; i < 64; i++) 
+setGrid(gridValue.value);
+
+btn.onclick          = () => clearGrid();
+gridValue.onchange   = () => clearGrid();
+
+function clearGrid () 
 {
-    for (let j = 0; j < 64; j++) 
+    grid.innerHTML = '';
+    setGrid(gridValue.value);
+};
+
+function randomColor() 
+{
+    return Math.floor(Math.random() * 256);
+};
+
+function changeColor(e) 
+{
+    if (random.checked === false)
+        e.target.style.backgroundColor = colorPicker.value;
+    else
+        e.target.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+};
+
+function setGrid(size) 
+{
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows    = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) 
     {
-        let div = document.createElement('div');
-        div.style.gridRow = i;
-        div.style.gridColumn = j;
-        board.appendChild(div);
-    }
-}
+       const gridElement = document.createElement('div');
+
+       gridElement.classList.add('grid-element');
+       gridElement.addEventListener('mouseover', changeColor);
+       grid.appendChild(gridElement)
+    };    
+};
